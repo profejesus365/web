e9beae2577a6daa59301ab26ac5386cffb66c56f
Profe. Jesús Álvarez <jalvarezsaez@outlook.com>
2026-09-18 13:14:52 -0500

    Add files via upload

--- enlaces-academia.js
+++ enlaces-academia.js
@@ -10,6 +10,6 @@
    funcionan igual sin necesidad de publicar el sitio.
    ========================================================================== */
 window.ENLACES_ACADEMIA = {
-  escuela: 'http://192.168.1.3:5050/estudiante/',
+  escuela: 'http://192.168.1.4:5174',
   web: 'https://profejesus365.github.io/code/',
 }
